f28a471d03cdd469363bd8cdefdc8bbf89430fde
mikaeltaylor <mikael.e.taylor@gmail.com>
2016-05-16 07:52:50 -0500

    Random Number IO Commit

---
+++ RandomNumberIO/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace RandomNumberIO
+{
+    partial class RandomNumberIOForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuantityInput = new System.Windows.Forms.TextBox();
+            this.lblQuantityInput = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lstFileOutput = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtQuantityInput
+            // 
+            this.txtQuantityInput.Location = new System.Drawing.Point(154, 25);
+            this.txtQuantityInput.Name = "txtQuantityInput";
+            this.txtQuantityInput.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantityInput.TabIndex = 0;
+            this.txtQuantityInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQuantityInput_KeyPress);
+            // 
+            // lblQuantityInput
+            // 
+            this.lblQuantityInput.AutoSize = true;
+            this.lblQuantityInput.Location = new System.Drawing.Point(74, 9);
+            this.lblQuantityInput.Name = "lblQuantityInput";
+            this.lblQuantityInput.Size = new System.Drawing.Size(265, 13);
+            this.lblQuantityInput.TabIndex = 1;
+            this.lblQuantityInput.Text = "How many random numbers would you like generated?";
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Location = new System.Drawing.Point(92, 51);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOpen.Location = new System.Drawing.Point(173, 51);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lstFileOutput
+            // 
+            this.lstFileOutput.FormattingEnabled = true;
+            this.lstFileOutput.Location = new System.Drawing.Point(15, 88);
+            this.lstFileOutput.Name = "lstFileOutput";
+            this.lstFileOutput.Size = new System.Drawing.Size(381, 160);
+            this.lstFileOutput.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Location = new System.Drawing.Point(255, 50);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // RandomNumberIOForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 264);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstFileOutput);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblQuantityInput);
+            this.Controls.Add(this.txtQuantityInput);
+            this.Name = "RandomNumberIOForm";
+            this.Text = "Random Number Reader/Writer v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuantityInput;
+        private System.Windows.Forms.Label lblQuantityInput;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox lstFileOutput;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ RandomNumberIO/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+// Program    :   Random Number Generator/Reader/Writer
+// Developer  :   Mikael Taylor
+// Date       :   2/26/2016
+// Description:   This program generates random numbers based on user input for the
+//                quantity desired.  It also uses a Save dialog, as well as an Open
+//                dialog in order to save a list of random numbers, or to open a list
+//                of random numbers.
+namespace RandomNumberIO
+{
+    public partial class RandomNumberIOForm : Form
+    {//Load the form, and get everything ready.
+        int quantityInput;//Variable for quantity user input for the random numbers.
+        Random random = new Random();//Random variable for our random number.
+        int randomNumber;//Variable to hold the random number until it's written.
+        public RandomNumberIOForm()
+        {
+            InitializeComponent();
+        }//Initialize the form's components
+        private void btnSave_Click(object sender, EventArgs e)
+        {//On clicking submit..
+            if (txtQuantityInput.Text != null && int.Parse(txtQuantityInput.Text) > 0)
+            {
+                try
+                {//Try to..
+                    quantityInput = int.Parse(txtQuantityInput.Text);
+                    //Bring in user input for the quantity to be written.
+                    int x = 0;
+                    //Start x at zero, so that during our while loop it will continue to generate and write the random numbers into a file.
+                    saveFileDialog.Filter = "Text Files(TXT)|*.TXT";
+                    saveFileDialog.ShowDialog();
+                    //Ask the user where to save the file, and what to name it.
+                    string file = saveFileDialog.FileName + ".txt";
+                    //Put the filename in a variable so that we can use it.
+                    StreamWriter writer = new StreamWriter(file);
+                    //Use StreamWriter so that we can write to the file.
+                    while (++x < quantityInput + 1)
+                    {//begin while
+                        //Generate Random Number
+                        randomNumber = random.Next(0, 100);
+                        //Write Random Number to Specified File
+                        writer.WriteLine(randomNumber);
+                    }//end while
+                    writer.Close();
+                }//end try
+                catch (Exception ex)
+                {//If an exception occurs..
+                    MessageBox.Show("Error Code: " + ex);
+                    //Tell me what happened.
+                }//close catch
+            }//end if
+            else
+            {//Otherwise, if the quantity is null, or less than 1
+                MessageBox.Show("Please enter a valid, non-negative quantity.");
+                txtQuantityInput.Text = "";
+            }//end else
+        }//end btnSave_Click
+        private void btnOpen_Click(object sender, EventArgs e)
+        {//When the user clicks the open button..
+            openFileDialog1.Filter = "Text Files (TXT)|*.TXT";
+            openFileDialog1.ShowDialog();
+            //Ask the user where to open the file..
+            string file = openFileDialog1.FileName;
+            //Keep the file name in a variable so we can use it.
+            if (file != null)
+            {
+                StreamReader reader = new StreamReader(file);
+                //Set up our stream reader to the file's location.
+                string line = reader.ReadLine();
+                //Read the first line of the file.
+                while (line != null)
+                {//While the line isn't empty
+                    lstFileOutput.Items.Add(line);
+                    //Write the line into our listbox control for output.
+                    line = reader.ReadLine();
+                    //Check to see if the next line is empty or not.
+                }//end while
+            }
+            else
+            {
+                MessageBox.Show("If you don't select a file, we cannot open it.");
+            }
+        }//end btnOpen_Click
+        private void txtQuantityInput_KeyPress(object sender, KeyPressEventArgs e)
+        {//Keypress validation for our txtQuanityInput textbox.
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {//If the key pressed isn't a number, or a control key(e.g. backspace, enter, etc.
+                e.Handled = true;//Then we have an error.
+                MessageBox.Show("You'll need to enter a valid, non-negative whole number.");
+            }//end if
+        }//end keypress validation
+        private void btnClear_Click(object sender, EventArgs e)
+        {//On clicking the clear button..
+            lstFileOutput.Items.Clear();
+            //Clear the list box
+        }//end Clear button
+    }//end RandomNumberIO form.
+}//end program
